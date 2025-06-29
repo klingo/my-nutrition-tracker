8663a3909c01920e2a3947e37dad47bc4a90ef90
Fabian Schär <klingo@klingonium.com>
2025-06-29 23:32:30 +0200

    Add MessageBox component with success, info, warning, and error types

---
+++ client/src/common/components/MessageBox/MessageBox.js
@@ -0,0 +1,53 @@
+import styles from './MessageBox.module.css';
+import BaseComponent from '@core/base/BaseComponent';
+
+/**
+ * Represents a customizable MessageBox component.
+ * @class MessageBox
+ */
+export default class MessageBox extends BaseComponent {
+    constructor({ text = '', type } = {}) {
+        super();
+
+        this.text = text;
+        this.type = this.#validateType(type);
+    }
+
+    #validateType(type) {
+        const validTypes = new Set(['success', 'info', 'warning', 'error']);
+        if (!validTypes.has(type)) {
+            throw new Error(`Invalid messagebox type "${type}". Must be one of: ${Array.from(validTypes).join(', ')}]`);
+        }
+        return type;
+    }
+
+    render() {
+        this.element = this.#createMessageBox();
+        this.#addIcon(this.element);
+        this.#addText(this.element);
+        return this.element;
+    }
+
+    #createMessageBox() {
+        const element = document.createElement('div');
+        element.classList.add(styles.messageBox, styles[this.type]);
+        return element;
+    }
+
+    #addIcon(element) {
+        const iconElement = document.createElement('div');
+        iconElement.classList.add(styles.icon);
+        element.appendChild(iconElement);
+    }
+
+    #addText(element) {
+        const textSpan = document.createElement('span');
+        textSpan.textContent = this.text;
+        element.appendChild(textSpan);
+    }
+
+    updateText(newText) {
+        this.text = newText;
+        this.element.querySelector('span').textContent = this.text;
+    }
+}
--- client/src/common/components/index.js
+++ client/src/common/components/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './Button/index.js';
 export { default as Input } from './Input/index.js';
 export { default as Loader } from './Loader/index.js';
+export { default as MessageBox } from './MessageBox/index.js';
 export { default as Navigation } from './Navigation/index.js';
 // ... other UI components
