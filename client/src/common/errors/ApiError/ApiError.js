ef70532d0f4e4143f859503b8e91d93def4c5585
Fabian Schär <klingo@klingonium.com>
2025-07-08 00:00:04 +0200

    Ensure `ApiError` properly assigns the message property

--- client/src/common/errors/ApiError/ApiError.js
+++ client/src/common/errors/ApiError/ApiError.js
@@ -2,6 +2,7 @@ class ApiError extends Error {
     constructor(message, status, response) {
         super(message);
         this.name = 'ApiError';
+        this.message = message;
         this.status = status;
         this.response = response;
     }
