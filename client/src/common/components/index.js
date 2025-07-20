719cb2abbf3733358347a75d02acbd4b898ef138
Fabian Schär <klingo@klingonium.com>
2025-07-20 22:56:45 +0200

    feat(components): export reusable Link component in index file

--- client/src/common/components/index.js
+++ client/src/common/components/index.js
@@ -3,6 +3,7 @@ export { default as ContentBlock } from './ContentBlock/index.js';
 export { default as ExpandableContainer } from './ExpandableContainer/index.js';
 export { default as Fieldset } from './Fieldset/index.js';
 export { default as Input } from './Input/index.js';
+export { default as Link } from './Link/index.js';
 export { default as Loader } from './Loader/index.js';
 export { default as MasonryContainer } from './MasonryContainer/index.js';
 export { default as MessageBox } from './MessageBox/index.js';
